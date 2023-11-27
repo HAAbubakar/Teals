be7948b0c809ee05a9efa697fbc620f2c1035423
HAAbubakar <106036192+HAAbubakar@users.noreply.github.com>
2023-11-27 21:14:42 +0500

    message

---
+++ assets/liquid-slider/mainjs.js
@@ -0,0 +1,4 @@
+$('#main-slider').liquidSlider({
+    slideEaseDuration: 1000, // Change this value to make the transitions faster
+    autoSlideInterval: 50000, // Adjust the auto-slide interval if needed
+  });
